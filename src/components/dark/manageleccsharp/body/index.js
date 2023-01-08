1320e056965c3f2dc77c2f25dc514dc4e411ed1e
Maxime Lameirinhas <maximelameirinhaspro@gmail.com>
2023-01-08 21:53:28 +0100

    debut integration manageleccsharp

--- src/components/dark/manageleccsharp/body/index.js
+++ src/components/dark/manageleccsharp/body/index.js
@@ -5,7 +5,19 @@ import "./body.css";
 function Body() {
     return (
     <div className="body">
-        <p>ceci sera la page du projet "menageleccsharp"</p>
+        <div className="about">
+            <p>
+                Manageleccsharp est un projet de creation d'un progitiel de gestion de commande
+                et de clients
+            </p><br/>
+            <p>
+                Il a été effectuer en octobre 2022 dans le cadre d'un projets de cours de le but 
+                de l'obtention du BTS SIO option SLAM
+            </p>
+            <p>
+                L'application a été réalisé en  C# WinForm avec le framework .NET 6
+            </p>
+        </div>
     </div>
     );
   }
