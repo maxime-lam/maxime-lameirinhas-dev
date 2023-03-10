49fa6ca7ed73791ce836ea143639cb14bfc6e62b
Maxime Lameirinhas <maximelameirinhaspro@gmail.com>
2023-03-10 23:19:05 +0100

    deploy portfolio

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -7,14 +7,6 @@ export const ProjectData=[
         image:"https://github.com/maxime-lam/project-img/blob/main/portfolio.PNG?raw=true",
         src:"/"
     },
-    {
-        id:2,
-        title:"Calculatrice en JavaScript",
-        about: "Ce projet a été réalisé en mai 2022. Il a été créé dans but de travailler le javascript et de créer une premiere 'application' en JS.",
-        tags:["Javascript"],
-        image:"https://github.com/maxime-lam/project-img/blob/main/calc.png?raw=true",
-        src:"/calcjs"
-    },
         {
         id:3,
         title:"Manageleccsharp",
