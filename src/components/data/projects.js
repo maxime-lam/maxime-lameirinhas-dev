ffddf8b90c5c68a1a694d12f42b5ac9a4223b879
Maxime Lameirinhas <maximelameirinhaspro@gmail.com>
2023-03-10 23:32:20 +0100

    test deploy 5

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,7 +5,7 @@ export const ProjectData=[
         about: "Ce projet a été réalisé en octobre 2022. Il a été créé dans le but de me créer un espace professionel de partage où je pourrais y publier mes projets et y afficher mes compétences.",
         tags:["ReactJS", "Javascript", "Html/Css"],
         image:"https://github.com/maxime-lam/project-img/blob/main/portfolio.PNG?raw=true",
-        src:"/"
+        src:"https://maxime-lam.github.io/maxime-lameirinhas-dev/"
     },
         {
         id:3,
@@ -13,6 +13,6 @@ export const ProjectData=[
         about: "Ce projet a été réalisé en septembre 2022. Il a été créé dans but de travailler le csharp et de créer un progitiel de gestion de commande.",
         tags:["C#"],
         image:"https://wichtech.com/wp-content/uploads/2016/09/noimg.jpg",
-        src:"/manageleccsharp"
+        src:"https://maxime-lam.github.io/maxime-lameirinhas-dev/manageleccsharp"
     }
 ];
