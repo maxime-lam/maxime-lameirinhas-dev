29475a25a4265dc3ab8b3a8b9d19c70848d4fa9d
Maxime Lameirinhas <maximelameirinhaspro@gmail.com>
2023-03-11 10:58:00 +0100

    test deploy 6

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -7,12 +7,4 @@ export const ProjectData=[
         image:"https://github.com/maxime-lam/project-img/blob/main/portfolio.PNG?raw=true",
         src:"https://maxime-lam.github.io/maxime-lameirinhas-dev/"
     },
-    {
-        id:2,
-        title:"Manageleccsharp",
-        about: "Ce projet a été réalisé en septembre 2022. Il a été créé dans but de travailler le csharp et de créer un progitiel de gestion de commande.",
-        tags:["C#"],
-        image:"https://wichtech.com/wp-content/uploads/2016/09/noimg.jpg",
-        src:"https://maxime-lam.github.io/maxime-lameirinhas-dev/manageleccsharp"
-    }
 ];
